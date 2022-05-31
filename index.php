65e3f71c33368ee206d62e6ef5d6bde971a07168
chanaka <chanaka@itplace.com>
2022-05-31 09:36:42 +0530

    change csv configs and read write informations

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
     session_start();
     require_once './library/config.php';
 
-    $connection = new createConnection();
-    $connection->connectToDatabase();
-
     $currentDate = date('Ymd');
     $csvDirectory = 'csv/';
     if (!is_dir($csvDirectory)) {
@@ -14,70 +11,86 @@
     $csvID = NULL;
 
     //GET LAST FEED ID
-    $query = "SELECT * FROM feed ORDER BY id DESC LIMIT 1";
-    $result = mysqli_query($connection->myconn, $query) or die(mysqli_error($connection->myconn));
+    $query = "SELECT * FROM feed WHERE `status`= 'active' ORDER BY id";
+    $result = $dbc->query($query) or die ($dbc->error . " (".$dbc->errno.")");
+    // $result = mysqli_query($connection->myconn, $query) or die(mysqli_error($connection->myconn));
     if ($result->num_rows > 0) {
-        $row = mysqli_fetch_assoc($result);
-        $csvURL = $row['url'];
-        $csvID = $row['id'];
-    } 
+        while($row = mysqli_fetch_assoc($result)){
 
-    if($csvURL == ""){
-        echo 'Cannot find any url in your database';
-        exit;
-    }
+            
+            $csvURL = $row['url'];
+            $csvID = $row['id'];
 
-    //DOWNLOAD CSV FILE
-    $source = file_get_contents($csvURL);
-    file_put_contents($csvDirectory.$currentDate.'.csv', $source);
+            //DOWNLOAD CSV FILE
+            $source = file_get_contents($csvURL);
+            file_put_contents($csvDirectory.$currentDate.'.csv', $source);
 
-    //CHECK FILE AVAILABILITY
-    $headerLine = true;
-    if(file_exists($csvDirectory.$currentDate.'.csv')){
-        $row = 0;
-        if (($handle = fopen($csvDirectory.$currentDate.'.csv', "r")) !== FALSE) {
+            //CHECK FILE AVAILABILITY
+            $headerLine = true;
+            if(file_exists($csvDirectory.$currentDate.'.csv')){
+                $count = 0;
+                if (($handle = fopen($csvDirectory.$currentDate.'.csv', "r")) !== FALSE) {
 
-            $date_time_inserted = "";
-            $date_time_updated = "";
-            $c_id = NULL;
-            $c_title = "";
-            $c_description = "";
-            $c_image = "";
+                    $date_time_inserted = "";
+                    $date_time_updated = "";
+                    $c_id = NULL;
+                    $c_unique_id = NULL;
+                    $c_title = "";
+                    $c_description = "";
+                    $c_image = "";
 
-            while (($data = fgetcsv($handle, null, ';')) !== FALSE) {
-                $num = count($data);
-                if($headerLine) { 
-                    $headerLine = false; 
-                } else {
-                    $row++;
+                    $csv = array();
+                    $columnnames = fgetcsv($handle, null, ';');
+                    while (($data = fgetcsv($handle, null, ';')) !== FALSE) {
+                        $num = count($data);
+                        if($headerLine) { 
+                            $headerLine = false; 
+                        } else {
+                            $count++;
+                            $newRow = array_combine($columnnames, $data);
+                            $csv[] = $newRow;
 
-                    $date_time_inserted = mysqli_real_escape_string($connection->myconn, $data[10]);
-                    $date_time_updated = mysqli_real_escape_string($connection->myconn, $data[11]);
-                    $c_id = mysqli_real_escape_string($connection->myconn, $data[0]);
-                    $c_title = mysqli_real_escape_string($connection->myconn, $data[1]);
-                    $c_description = mysqli_real_escape_string($connection->myconn, $data[4]);
-                    $c_image = mysqli_real_escape_string($connection->myconn, $data[5]);
+                            foreach($csv as $key=>$val){
 
-                    $ckQuery = "SELECT * FROM feed_content WHERE c_id = '$c_id'";
-                    $ckResult = mysqli_query($connection->myconn, $ckQuery) or die(mysqli_error($connection->myconn));
-                    if ($ckResult->num_rows == 0) {
-                        $insertQuery = "INSERT INTO feed_content (feed_id,`status`,date_time_inserted,date_time_updated,c_id,c_title,c_description,c_image) VALUES ('$csvID','','$date_time_inserted','$date_time_updated','$c_id','$c_title','$c_description','$c_image')";
-                        $resultQuery = mysqli_query($connection->myconn, $insertQuery) or die(mysqli_error($connection->myconn));
-                    } else {
-                        $updateQuery = "UPDATE feed_content SET 
-                        feed_id = '$csvID', 
-                        date_time_inserted = '$date_time_inserted',
-                        date_time_updated = '$date_time_updated',
-                        c_title = '$c_title',
-                        c_description = '$c_description',
-                        c_image = '$c_image' WHERE c_id = '$c_id'";
-                        $resultUpdateQuery = mysqli_query($connection->myconn, $updateQuery) or die(mysqli_error($connection->myconn));
+                                if(isset($row['mapping_c_id']) && $row['mapping_c_id']!="" && isset($val[$row['mapping_c_id']])){
+                                $c_id = mysqli_real_escape_string($dbc, $val[$row['mapping_c_id']]);
+                                }
+                                if(isset($row['mapping_c_title']) && $row['mapping_c_title']!="" && isset($val[$row['mapping_c_title']])){
+                                $c_title = mysqli_real_escape_string($dbc, $val[$row['mapping_c_title']]);
+                                }
+                                if(isset($row['mapping_c_description']) && $row['mapping_c_description']!="" && isset($val[$row['mapping_c_description']])){
+                                    $c_description = mysqli_real_escape_string($dbc, $val[$row['mapping_c_description']]);
+                                }
+                                if(isset($row['mapping_c_image']) && $row['mapping_c_image']!="" && isset($val[$row['mapping_c_image']])){
+                                    $c_image = mysqli_real_escape_string($dbc, $val[$row['mapping_c_image']]);
+                                }
+                                if(isset($row['mapping_c_unique_id']) && $row['mapping_c_unique_id']!="" && isset($val[$row['mapping_c_unique_id']])){
+                                    $c_unique_id = mysqli_real_escape_string($dbc, $val[$row['mapping_c_unique_id']]);
+                                }
+                                $date_time_inserted = date('Y-m-d H:i:s');
+                                $date_time_updated = date('Y-m-d H:i:s');
+
+                                $ckQuery = "SELECT * FROM feed_content WHERE c_id = '$c_id'";
+                                $ckResult = $dbc->query($ckQuery) or die ($dbc->error . " (".$dbc->errno.")");
+                                if ($ckResult->num_rows == 0) {
+                                    $insertQuery = "INSERT INTO feed_content (feed_id,`status`,date_time_inserted,date_time_updated,c_id,c_title,c_description,c_image,c_unique_id) VALUES ('$csvID','active','$date_time_inserted','$date_time_updated','$c_id','$c_title','$c_description','$c_image','$c_unique_id')";
+                                    $resultQuery = $dbc->query($insertQuery) or die ($dbc->error . " (".$dbc->errno.")");
+                                } else {
+                                    $updateQuery = "UPDATE feed_content SET 
+                                    feed_id = '$csvID', 
+                                    date_time_updated = '$date_time_updated',
+                                    c_title = '$c_title',
+                                    c_description = '$c_description',
+                                    c_unique_id = '$c_unique_id',
+                                    c_image = '$c_image' WHERE c_id = '$c_id'";
+                                    $resultUpdateQuery = $dbc->query($updateQuery) or die ($dbc->error . " (".$dbc->errno.")");
+                                }
+                            }
+                        }
                     }
-
-                    
+                    fclose($handle);
                 }
             }
-            fclose($handle);
         }
     }
     echo 'CSV import process has been completed';
--- library/config.php
+++ library/config.php
@@ -1,26 +1,32 @@
 <?php
-class createConnection  { // make connection
-    var $host = 'localhost';
-    var $user = 'root';
-    var $pass = '';
-    var $db = 'flowspot_db';
-    var $myconn;
 
-    function connectToDatabase() {
-        $con = mysqli_connect($this->host, $this->user, $this->pass, $this->db);
-        if (!$con) {
-            die('Could not connect to database!');
-        } else {
-            $this->myconn = $con;
-//            echo 'Connection established!';
-        }
-        return $this->myconn;
+$json = '{
+"CTOOLS_DB_HOST" : "localhost",
+"CTOOLS_DB_USER" : "root",
+"CTOOLS_DB_PASS" : "",
+"CTOOLS_DB_NAME" : "flowspot_db"
+}';
+
+$defaultConfig  = json_decode($json);
+foreach($defaultConfig as $key=>$value){
+    if(!getenv($key)){
+        putenv($key.'='.$value);
     }
-
-    function close() {
-        mysqli_close($this->myconn);
-//        echo 'Connection closed!';
-    }
-
 }
-?>
+
+
+$host = getenv('CTOOLS_DB_HOST');
+$user = getenv('CTOOLS_DB_USER');
+$pass = getenv('CTOOLS_DB_PASS');
+$name = getenv('CTOOLS_DB_NAME');
+
+// DB_HOST - DB_USER - DB_PASSWORD - DB_NAME
+$dbc = @mysqli_connect($host, $user, $pass, $name);
+
+if (!$dbc) {
+    http_response_code(500);
+    die('Error connecting to ' . $host . ' ('. mysqli_connect_errno() .')  from ' . $_SERVER["SCRIPT_FILENAME"] . ' Error:'. mysqli_connect_error() . PHP_EOL);
+    exit();
+}
+
+$dbc->set_charset('utf8');
